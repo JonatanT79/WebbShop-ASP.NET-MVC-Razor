4f01b74b3d9a14094473c11830d193b848bf8477
Jonathan Tran <jonathan.t79@hotmail.com>
2020-04-27 20:53:19 +0200

    Major Changes 1.0

--- WebbShop/Controllers/ProductsController.cs
+++ WebbShop/Controllers/ProductsController.cs
@@ -12,14 +12,68 @@ namespace WebbShop.Controllers
         public IActionResult IndexProducts()
         {
             Products _products = new Products();
-            _products.Name = "Gaming Dator";
-            _products.Description = "Stationär";
-            _products.Price = 14.999M;
 
-            _products.Productlist.Add(_products);
+            Products _products1 = new Products();
+            _products1.ID = 1;
+            _products1.Name = "Gaming Dator";
+            _products1.Description = "Stationär";
+            _products1.Price = 14999M;
+
+            Products _products2 = new Products();
+            _products2.ID = 2;
+            _products2.Name = "TV";
+            _products2.Description = "55 Tum";
+            _products2.Price = 8999M;
+
+            Products _products3 = new Products();
+            _products3.ID = 3;
+            _products3.Name = "Hörlurar";
+            _products3.Description = "Iphone";
+            _products3.Price = 799M;
+
+            _products.Productlist.Add(_products1);
+            _products.Productlist.Add(_products2);
+            _products.Productlist.Add(_products3);
+
             return View(_products);
+        }
+        public IActionResult ViewProduct(int ID)
+        {
+            //Test data, OBS ifsatsen ska bytas ut mot LINQ senare när databasen finns
+            ProductDetails _productdetails = new ProductDetails();
+            if(ID == 1)
+            {
+                _productdetails.Name = "Dator";
+                _productdetails.Description = "Stationär";
+                _productdetails.Price = 14999M;
+                var DateAndTime = DateTime.Now;
+                _productdetails.Release = DateAndTime.Date;
+                _productdetails.Maker = "MSI";
+            }
+            else if(ID == 2)
+            {
+                _productdetails.Name = "TV";
+                _productdetails.Description = "55 Tum";
+                _productdetails.Price = 8999M;
+                var DateAndTime = DateTime.Now;
+                _productdetails.Release = DateAndTime.Date;
+                _productdetails.Maker = "Philips";
+            }
+            else if(ID == 3)
+            {
+                _productdetails.Name = "Hörlurar";
+                _productdetails.Description = "Iphone";
+                _productdetails.Price = 799M;
+                var DateAndTime = DateTime.Now;
+                _productdetails.Release = DateAndTime.Date;
+                _productdetails.Maker = "Apple";
+            }
+
+            return View(_productdetails);
         }
     }
 }
 
 //TODO lägg till orderbekräftelsesida och tillhörande knappar
+//TODO (senare) lägg till admin sida för att skapa/editera/ta bort produkter
+//Lagerstatus
---
+++ WebbShop/Models/ProductDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebbShop.Models
+{
+    public class ProductDetails
+    {
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Release { get; set; }
+        public string Maker { get; set; }
+    }
+}
