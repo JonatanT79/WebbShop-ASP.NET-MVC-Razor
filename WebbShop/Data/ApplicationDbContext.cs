825686a2a2af82652da7889446b895e7a0158cc3
Jonathan Tran <jonathan.t79@hotmail.com>
2020-04-27 13:54:08 +0200

    Add project files.

---
+++ WebbShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebbShop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            //ändra databasnamnet
+            optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDB; Database = TranbarDB; Trusted_Connection = True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelbBuilder)
+        {
+            base.OnModelCreating(modelbBuilder);
+        }
+    }
+}
