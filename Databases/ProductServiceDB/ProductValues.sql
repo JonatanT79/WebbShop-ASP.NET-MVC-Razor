63c341bc388dea191bf03fb4fd15de829de715a9
Jonathan Tran <jonathan.t79@hotmail.com>
2020-05-08 19:40:29 +0200

    Created ProductServiceDB

---
+++ Databases/ProductServiceDB/ProductValues.sql
@@ -0,0 +1,6 @@
+insert into Products([Name], [Description], Price, Maker)
+values
+('PC', 'Deskop', 14999, 'Acer'),
+('TV', '55 Tum', 8999, 'Philips'),
+('Headphones', 'Iphone', 799, 'Apple'),
+('Keyboard', 'Gaming', 1050, 'Razor')
--- Product.API/Data/ProductContext.cs
+++ Product.API/Data/ProductContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Product.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,13 @@ namespace Product.API.Data
 {
     public class ProductContext : DbContext
     {
+        DbSet<Products> Products { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDB; Database = ProductService; Trusted_Connection = True;");
+        }
     }
 }
