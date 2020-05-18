c1d791cdb1b84406755f2ee9e5900daec193860c
JonatanT79 <56258700+JonatanT79@users.noreply.github.com>
2020-05-19 01:14:10 +0200

    Updated SQL Script

--- ProductValues.sql
+++ ProductValues.sql
@@ -1,14 +1,6 @@
-insert into ProductBrand(Brand)
+insert into Products([Name], [Description], Price, InStock, Maker)
 values
-('Acer'),
-('Philips'),
-('Apple'),
-('Razor')
-
-insert into Products([Name], [Description], Price, InStock, ProductBrandID)
-values
-('PC', 'Deskop', 14999, 3, 1),
-('TV', '55 Tum', 8999, 2, 2),
-('Headphones', 'Iphone', 799, 5, 3),
-('Keyboard', 'Gaming', 1050, 4, 4)
-
+('PC', 'Deskop', 14999, 3, 'Acer'),
+('TV', '55 Tum', 8999, 2, 'Philips'),
+('Headphones', 'Iphone', 799, 5, 'Apple'),
+('Keyboard', 'Gaming', 1050, 4, 'Razor')
